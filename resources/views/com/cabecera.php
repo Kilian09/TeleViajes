c0add139c91d57bc793a2c892cc2556f003e3dce
Kilian <kilian.garcia106@alu.ulpgc>
2020-12-13 13:01:32 +0000

    Separada cabecera  pieDePagina en dos archivos

---
+++ resources/views/com/cabecera.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-dark navbar-expand-md text-white bg-dark navigation-clean-button" style="width: 100%;height: 18%;">
+    <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><a class="navbar-brand" href="index.php"><img src="assets/img/logo%20(1).png" style="height: 75px;background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;);width: 200px;"></a><a class="navbar-brand" href="#"></a>
+        <div class="collapse navbar-collapse" id="navcol-1" style="color: #f2f5f8;">
+            <ul class="nav navbar-nav mr-auto">
+                <li class="nav-item"></li>
+                <li class="nav-item"></li>
+                <li class="nav-item"><a class="nav-link" href="cruceros.html" style="color: var(--light);">Cruceros</a></li>
+                <li class="nav-item"><a class="nav-link" href="paquetes.html" style="color: var(--light);">Paquetes</a></li>
+                <li class="nav-item"><a class="nav-link" href="actividades.html" style="color: var(--light);">Actividades</a></li>
+                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="grupos.html" style="color: var(--light);">Grupos&nbsp;</a>
+                    <div class="dropdown-menu"><a class="dropdown-item" href="#">Escolares</a><a class="dropdown-item" href="#">Universitarios</a><a class="dropdown-item" href="#">Ancianos</a><a class="dropdown-item" href="#">Familia</a></div>
+                </li>
+            </ul><span class="d-inline navbar-text actions"> <a class="d-inline login" href="login.html">Iniciar Sesión</a><a class="btn btn-light d-inline action-button" role="button" href="register.html">Registrarse</a></span>
+        </div>
+    </div>
+</nav>
+
+
