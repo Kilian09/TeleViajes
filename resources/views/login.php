9dca91b91858c9096217f47721a8d01d55553ba7
Kilian <kilian.garcia106@alu.ulpgc>
2020-12-13 13:09:19 +0000

    Implementado cabecera y pieDePagina

--- resources/views/login.php
+++ resources/views/login.php
@@ -36,22 +36,11 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-dark navbar-expand-md text-white bg-dark navigation-clean-button" style="width: 100%;height: 18%;">
-        <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><a class="navbar-brand" href="index.html"><img src="assets/img/logo%20(1).png" style="height: 75px;background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;);width: 200px;"></a><a class="navbar-brand" href="#"></a>
-            <div class="collapse navbar-collapse" id="navcol-1" style="color: #f2f5f8;">
-                <ul class="nav navbar-nav mr-auto">
-                    <li class="nav-item"></li>
-                    <li class="nav-item"></li>
-                    <li class="nav-item"><a class="nav-link" href="cruceros.html" style="color: var(--light);">Cruceros</a></li>
-                    <li class="nav-item"><a class="nav-link" href="paquetes.html" style="color: var(--light);">Paquetes</a></li>
-                    <li class="nav-item"><a class="nav-link" href="actividades.html" style="color: var(--light);">Actividades</a></li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="grupos.html" style="color: var(--light);">Grupos&nbsp;</a>
-                        <div class="dropdown-menu"><a class="dropdown-item" href="#">Escolares</a><a class="dropdown-item" href="#">Universitarios</a><a class="dropdown-item" href="#">Ancianos</a><a class="dropdown-item" href="#">Familia</a></div>
-                    </li>
-                </ul><span class="d-inline navbar-text actions"> <a class="d-inline login" href="login.php">Iniciar Sesión</a><a class="btn btn-light d-inline action-button" role="button" href="register.html">Registrarse</a></span>
-            </div>
-        </div>
-    </nav>
+
+<?php
+include "com/cabecera.php"
+?>
+
     <div class="login-dark" style="background: url(&quot;assets/img/bledd.jpg&quot;) center / cover;">
         <form method="post">
             <h2 class="sr-only">Login Form</h2>
@@ -61,11 +50,11 @@
             <div class="form-group"><button class="btn btn-primary btn-block" type="submit">IniciAR sesion</button></div><a class="forgot" href="register.html">No te has registrado aún?</a>
         </form>
     </div>
-    <footer class="bg-dark" style="padding: 25px;">
-        <div class="container text-center">
-            <p>Copyright ©&nbsp;TELEVIAJES 2020</p>
-        </div>
-    </footer>
+
+<?php
+include "com/pieDePagina.php";
+?>
+
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
