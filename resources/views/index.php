bf48f3105031d1389d6804f45d8af7b19773aa4d
Kilian <kilian.garcia106@alu.ulpgc>
2020-12-13 12:59:23 +0000

    Añadido el index.php, eliminado pruebba.php y welcome.blade.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - TELEVIAJES</title>
+    <title>Inicio</title>
+
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin:700">
@@ -35,23 +35,14 @@
     <link rel="stylesheet" href="assets/css/Simple-Slider.css">
 </head>
 
+
+
 <body id="page-top">
-    <nav class="navbar navbar-dark navbar-expand-md text-white bg-dark navigation-clean-button" style="width: 100%;height: 18%;">
-        <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><a class="navbar-brand" href="index.php"><img src="assets/img/logo%20(1).png" style="height: 75px;background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;);width: 200px;"></a><a class="navbar-brand" href="#"></a>
-            <div class="collapse navbar-collapse" id="navcol-1" style="color: #f2f5f8;">
-                <ul class="nav navbar-nav mr-auto">
-                    <li class="nav-item"></li>
-                    <li class="nav-item"></li>
-                    <li class="nav-item"><a class="nav-link" href="cruceros.html" style="color: var(--light);">Cruceros</a></li>
-                    <li class="nav-item"><a class="nav-link" href="paquetes.html" style="color: var(--light);">Paquetes</a></li>
-                    <li class="nav-item"><a class="nav-link" href="actividades.html" style="color: var(--light);">Actividades</a></li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="grupos.html" style="color: var(--light);">Grupos&nbsp;</a>
-                        <div class="dropdown-menu"><a class="dropdown-item" href="#">Escolares</a><a class="dropdown-item" href="#">Universitarios</a><a class="dropdown-item" href="#">Ancianos</a><a class="dropdown-item" href="#">Familia</a></div>
-                    </li>
-                </ul><span class="d-inline navbar-text actions"> <a class="d-inline login" href="login.html">Iniciar Sesión</a><a class="btn btn-light d-inline action-button" role="button" href="register.html">Registrarse</a></span>
-            </div>
-        </div>
-    </nav>
+
+<?php
+include "com/cabecera.php"
+?>
+
     <header class="masthead" style="background: url(&quot;assets/img/ini.jpg&quot;) left / auto repeat;">
         <div id="inicio" class="intro-body">
             <div class="container">
@@ -171,11 +162,11 @@
         </div><a class="btn btn-link btn-circle" role="button" href="#inicio"><i class="fa fa-angle-double-up animated"></i></a>
     </section>
     <div class="map-clean"><iframe allowfullscreen="" frameborder="0" src="https://cdn.bootstrapstudio.io/placeholders/map.html" width="100%" height="450"></iframe></div>
-    <footer class="bg-dark" style="padding: 25px;">
-        <div class="container text-center">
-            <p>Copyright ©&nbsp;TELEVIAJES 2020</p>
-        </div>
-    </footer>
+
+<?php
+include "com/pieDePagina.php";
+?>
+
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
