26a771a94d40d38abedb1846bbadb8ba2fd1e299
Fernando Santana <feersantana5@gmail.com>
2020-12-15 03:45:27 +0000

    footer modification

--- resources/views/com/pieDePagina.php
+++ resources/views/com/pieDePagina.php
@@ -1,5 +1,38 @@
-<footer class="bg-dark" style="padding: 25px;">
-    <div class="container text-center">
-        <p>Copyright ©&nbsp;TELEVIAJES 2020</p>
+
+
+
+
+
+<footer style="margin: 0;" class=" bg-dark ">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 text-center footer-navigation">
+            <h3><img src="assets/img/logo%20(1).png" style="height: 75px;background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;);width: 200px;"></h3>
+            <p class="links"><a href="#">Inicio</a><strong> · </strong> <a href="#">Contacto</a></p>
+            <p class="company-name">TeleViajes © 2020</p>
+        </div>
+
+        <div class="col-sm-6 col-md-4 footer-contacts">
+            <div>
+                <i class="fa fa-map-marker footer-contacts-icon"></i>
+                <a> Tafira Baja, Las Palmas Gran Canaria, España</a>
+            </div>
+            <div>
+                <i class="fa fa-phone footer-contacts-icon"></i> <a class="footer-center-info email text-left">+34 630 267 876</a>
+            </div>
+            <div>
+                <i class="fa fa-envelope footer-contacts-icon"></i> <a href="#" target="_blank">support@company.com</a>
+            </div>
+        </div>
+
+        <div class="col-md-4 footer-about">
+            <h4>Sobre la compañia</h4>
+            <p> Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet. </p>
+            <div class="social-links social-icons">
+                <a href="https://sites.google.com/view/televiajes/inicio"> <i class="fa fa-paperclip"></i></a>
+                <a href="https://github.com/Kilian09/TeleViajes"><i class="fa fa-github"></i></a>
+            </div>
+        </div>
+
     </div>
+
 </footer>
