4209b0ebec51fdc05fbf54ffac29f483d45d7dfb
Kilian <kilian.garcia106@alu.ulpgc>
2020-12-17 18:16:43 +0000

    Añadido actvidades.php

---
+++ resources/views/actividades.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>TELEVIAJES 1.9</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin:700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+</head>
+
+<body>
+    <div class="container" style="padding-top: 30px;">
+        <h1>las actividades más vendidas</h1>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <div class="box">
+                    <div class="box-img"><img src="assets/img/esqui.jpg" alt="Montañismo" style="text-align: center;"></div>
+                    <div class="box-content">
+                        <h4 class="title">Esqui</h4>
+                        <p class="description">Rellenar descripción</p>
+                        <ul class="social-links" style="text-align: center;">
+                            <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="box">
+                    <div class="box-img"><img src="assets/img/caribe.jpg" alt="Williamson"></div>
+                    <div class="box-content">
+                        <h4 class="title">Playas EXÓTICAS<br></h4>
+                        <p class="description">Rellenar Descripcion</p>
+                        <ul class="social-links" style="text-align: center;">
+                            <li style="text-align: left;"><a href="#" style="font-size: 30px;"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="box">
+                    <div class="box-img"><img src="assets/img/visitas%20guiadas.jpg" alt="Williamson"></div>
+                    <div class="box-content">
+                        <h4 class="title">visitas guiadas<br></h4>
+                        <p class="description">Rellenar descripción</p>
+                        <ul class="social-links" style="text-align: center;">
+                            <li><a href="#"><i class="fa fa-shopping-cart" style="text-align: right;font-size: 30px;"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="padding-top: 30px;">
+        <h1>las Actividades exclusivas</h1>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <div class="box">
+                    <div class="box-img"><img src="assets/img/lasvegas.jpg" alt="Montañismo" style="text-align: center;"></div>
+                    <div class="box-content">
+                        <h4 class="title">Casinos</h4>
+                        <p class="description">Rellenar descripción</p>
+                        <ul class="social-links" style="text-align: center;">
+                            <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="box">
+                    <div class="box-img"><img src="assets/img/spa.jpg" alt="Williamson"></div>
+                    <div class="box-content">
+                        <h4 class="title">Spa<br></h4>
+                        <p class="description">Rellenar Descripcion</p>
+                        <ul class="social-links" style="text-align: center;">
+                            <li style="text-align: left;"><a href="#" style="font-size: 30px;"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="box">
+                    <div class="box-img"><img src="assets/img/cena.jpeg" alt="Williamson"></div>
+                    <div class="box-content">
+                        <h4 class="title">Cena exclusiva<br></h4>
+                        <p class="description">Rellenar descripción</p>
+                        <ul class="social-links" style="text-align: center;">
+                            <li><a href="#"><i class="fa fa-shopping-cart" style="text-align: right;font-size: 30px;"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="padding-top: 30px;">
+        <h1>las actividades más salvajes</h1>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <div class="box">
+                    <div class="box-img"><img src="assets/img/escalada.jpg" alt="Montañismo" style="text-align: center;"></div>
+                    <div class="box-content">
+                        <h4 class="title">Escalada</h4>
+                        <p class="description">Rellenar descripción</p>
+                        <ul class="social-links" style="text-align: center;">
+                            <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="box">
+                    <div class="box-img"><img src="assets/img/paracaidismo.jpg" alt="Williamson"></div>
+                    <div class="box-content">
+                        <h4 class="title">Paracaidismo<br></h4>
+                        <p class="description">Rellenar Descripcion</p>
+                        <ul class="social-links" style="text-align: center;">
+                            <li style="text-align: left;"><a href="#" style="font-size: 30px;"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="box">
+                    <div class="box-img"><img src="assets/img/paintball.jpg" alt="Williamson"></div>
+                    <div class="box-content">
+                        <h4 class="title">Paintball<br></h4>
+                        <p class="description">Rellenar descripción</p>
+                        <ul class="social-links" style="text-align: center;">
+                            <li><a href="#"><i class="fa fa-shopping-cart" style="text-align: right;font-size: 30px;"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
