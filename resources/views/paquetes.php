eeb9485e0068da21058eb21fcbd63ebd44ef4db9
Kilian <kilian.garcia106@alu.ulpgc>
2020-12-17 18:12:41 +0000

    Añadido paquetes.php

---
+++ resources/views/paquetes.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>TELEVIAJES 1.9</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin:700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+</head>
+
+<body style="background: url(&quot;assets/img/intro-bg.jpg&quot;), #c9c9c9;">
+    <div class="container" style="margin-top: 8px;">
+        <div class="carousel slide" data-ride="carousel" id="carousel-1">
+            <div class="carousel-inner">
+                <div class="carousel-item active"><img class="img-thumbnail w-100 d-block" src="assets/img/el-origen-de-las-auroras-boreales-183012-1.jpg" alt="Slide Image"></div>
+                <div class="carousel-item"><img class="w-100 d-block" src="disneyland.jpg" alt="Slide Image"></div>
+                <div class="carousel-item"><img class="w-100 d-block" src="assets/img/praga.jpg" alt="Slide Image"></div>
+            </div>
+            <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev"><span class="carousel-control-prev-icon"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-slide="next"><span class="carousel-control-next-icon"></span><span class="sr-only">Next</span></a></div>
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-1" data-slide-to="1"></li>
+                <li data-target="#carousel-1" data-slide-to="2"></li>
+            </ol>
+        </div>
+    </div>
+    <div class="container" style="margin-top: 100px;">
+        <h1>Los paquetes más populares</h1>
+        <div class="card-group">
+            <div class="card"><img class="card-img-top w-100 d-block" src="assets/img/Aurora-boreal-300x200.jpg">
+                <div class="card-body">
+                    <h4 class="card-title" style="color: rgb(34,33,33);">Tour auroras&nbsp; en 3 días para 2 pers.</h4>
+                    <p class="card-text" style="color: rgb(21,21,21);"><br>Experimenta la naturaleza del Ártico mientras persigues este increíble espectáculo de luces en un tour con todo incluido para dos personas.&nbsp;<br>Si quieres aprovechar tu viaje por Islandia con aventuras y excursiones increíbles, no deberías perderte este paquete.<br><br></p>
+                    <p class="card-text" style="color: rgb(21,21,21);">&nbsp; Del 20.01.2021 al 23.01.2021</p>
+                    <p class="text-center bg-info card-text" style="color: rgb(21,21,21);">Por 400€</p><button class="btn btn-primary border rounded-pill" type="button" style="margin-left: 32%;">¡Reserva ya!</button>
+                </div>
+            </div>
+            <div class="card" style="margin-left: 0.5%;"><img class="card-img-top w-100 d-block" src="assets/img/pragra%20300x200.webp">
+                <div class="card-body">
+                    <h4 class="card-title" style="color: rgb(34,33,33);">Pragra y Budapest en 5 días para 2 pers.</h4>
+                    <p class="card-text" style="color: rgb(21,21,21);">Déjate seducir por su encanto y por sus innumerables muestras de belleza, palpables en sus monumentos y en sus calles.<br><br>Experimenta la naturaleza del Ártico mientras persigues este increíble espectáculo de luces en un tour con todo incluido para dos personas.&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br></p>
+                    <p class="card-text" style="color: rgb(21,21,21);">Del 01.02.2021 al 06.02.2021<br></p>
+                    <p class="text-center bg-info card-text" style="color: rgb(21,21,21);">Por 700€</p><button class="btn btn-primary border rounded-pill" type="button" style="margin-left: 32%;">¡Reserva ya!</button>
+                </div>
+            </div>
+            <div class="card" style="margin-left: 0.5%;"><img class="card-img-top w-100 d-block" src="assets/img/hero-background-travel.jpg">
+                <div class="card-body">
+                    <h4 class="card-title" style="color: rgb(34,33,33);">LONDRES, AMSTERDAM Y BERLIN en 9 DÍAS para 2 pers.<br></h4>
+                    <p class="card-text" style="color: rgb(21,21,21);">Le ofrecemos un paquete turístico donde podrá conocer más a fondo&nbsp;las principales ciudades europeas como Londres y Países Bajos.&nbsp;<br>Podrá&nbsp;visitar la Fábrica-Museo de Diamantes en Ámsterdam, el Memorial del Holocausto y el&nbsp;Museo del muro de Berlín.&nbsp; &nbsp; &nbsp;<br></p>
+                    <p class="card-text" style="color: rgb(21,21,21);">Del 10.02.2021 al 19.02.2021<br></p>
+                    <p class="text-center bg-info card-text" style="color: rgb(21,21,21);">Por 1000€</p><button class="btn btn-primary border rounded-pill" type="button" style="margin-left: 32%;">¡Reserva ya!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="margin-top: 100px;">
+        <h1>paquetes TEMÁTICOS</h1>
+        <div class="card-group">
+            <div class="card"><img class="card-img-top w-100 d-block" src="assets/img/disney%20300x200.jpg">
+                <div class="card-body">
+                    <h4 class="card-title" style="color: rgb(34,33,33);">DisneyLand 3 días para 2 Adultos y 2 niños.</h4>
+                    <p class="card-text" style="color: rgb(21,21,21);">De regreso a la magia, que mejor manera de vivir la fantasía, el descubrimiento, el mundo del espectáculo y la aventura en los parques temáticos y&nbsp;los hoteles&nbsp;en Disneyland.<br><br></p>
+                    <p class="text-left card-text" style="color: rgb(21,21,21);">Del 20.01.2021 al 23.01.2021</p>
+                    <p class="text-center bg-info card-text" style="color: rgb(21,21,21);">Por 900€</p><button class="btn btn-primary border rounded-pill" type="button" style="margin-left: 32%;">¡Reserva ya!</button>
+                </div>
+            </div>
+            <div class="card" style="margin-left: 0.5%;"><img class="card-img-top w-100 d-block" src="assets/img/Warner-300x200.jpg">
+                <div class="card-body">
+                    <h4 class="card-title" style="color: rgb(34,33,33);">Warner 2 días para 2 adultos y 2 niños.</h4>
+                    <p class="card-text" style="color: rgb(21,21,21);">Descubre sus cinco áreas temáticas: Hollywood Boulevard, Movie World Studios, Old West Territory, Cartoon Village y Super Heroes World, con decorados que te transportarán a Estados Unidos y a míticas escenas de cine y de dibujos animados.&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br></p>
+                    <p class="card-text" style="color: rgb(21,21,21);">Del 01.02.2021 al 03.02.2021<br></p>
+                    <p class="text-center bg-info card-text" style="color: rgb(21,21,21);">Por 700€</p><button class="btn btn-primary border rounded-pill" type="button" style="margin-left: 32%;">¡Reserva ya!</button>
+                </div>
+            </div>
+            <div class="card" style="margin-left: 0.5%;"><img class="card-img-top w-100 d-block" src="assets/img/siampark.jpg">
+                <div class="card-body">
+                    <h4 class="card-title" style="color: rgb(34,33,33);">Loro Park y Siam Park 2 DÍAS para 2 adultos y 2 niños.<br></h4>
+                    <p class="card-text" style="color: rgb(21,21,21);">Disfruta de lo mejor de ambos mundos: el terrenal y el<br>acuático. Descarga adrenalina en el reino acuático de Siam Park y haz nuevos<br>amigos en el salvaje Loro Parque. ¡Dos días insuperables!&nbsp; &nbsp; &nbsp;&nbsp;<br></p>
+                    <p class="card-text" style="color: rgb(21,21,21);">Del 10.02.2021 al 12.02.2021<br></p>
+                    <p class="text-center bg-info card-text" style="color: rgb(17,15,15);">Por 300€</p><button class="btn btn-primary border rounded-pill" type="button" style="margin-left: 32%;">¡Reserva ya!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
