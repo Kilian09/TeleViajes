c494653722097366ea05660b483ac009afd422bd
Kilian <kilian.garcia106@alu.ulpgc>
2020-12-17 18:39:28 +0000

    Añadido asset y cambios en universitarios.php

--- resources/views/universitarios.php
+++ resources/views/universitarios.php
@@ -4,17 +4,115 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Universitari@s</title>
+    <title>Universitarios</title>
 
     <?php include "com/estilos.php"?>
     <?php include "com/scripts.php"?>
 
 </head>
 
-<body style="background: url(&quot;assets/img/intro-bg.jpg&quot;), #c9c9c9;">
+<body>
 
     <?php include "com/cabecera.php"?>
 
+    <div>
+        <div id="box-2">
+            <h1 style="background: url(&quot;assets/img/ski.jpg&quot;) center / cover;">LOS MEJORES VIAJES</h1>
+        </div>
+        <div id="box-3">
+            <h1 style="background: url(&quot;assets/img/Parques-atracciones-Portugal-.jpg&quot;);background-size: cover;">LAS MEJORES EXPERIENCIAS</h1>
+        </div>
+        <div id="box-1">
+            <h1 style="background: url(&quot;assets/img/tomorrowland.jpg&quot;) center;">LAS MEJORES FIESTAS</h1>
+        </div>
+        <section class="text-center" style="margin-top: 20px;">
+            <div class="d-flex flex-row multiple-item-slider">
+                <div class="justify-content-center spacer-slider">
+                    <figure class="figure"><img class="img-fluid figure-img" src="https://picsum.photos/1080/1335?image=0" alt="alt text here">
+                        <figcaption class="figure-caption">Caption</figcaption>
+                    </figure>
+                </div>
+                <div class="justify-content-center spacer-slider">
+                    <figure class="figure"><img class="img-fluid figure-img mx-auto d-block" src="https://picsum.photos/1080/1335?image=1083" alt="alt text here">
+                        <figcaption class="figure-caption">Caption</figcaption>
+                    </figure>
+                </div>
+                <div class="justify-content-center spacer-slider">
+                    <figure class="figure"><img class="img-fluid figure-img" src="https://picsum.photos/1080/1335?image=1076" alt="alt text here">
+                        <figcaption class="figure-caption">Caption</figcaption>
+                    </figure>
+                </div>
+                <div class="justify-content-center spacer-slider">
+                    <figure class="figure"><img class="img-fluid figure-img" src="https://picsum.photos/1080/1335?image=1075" alt="alt text here">
+                        <figcaption class="figure-caption">Caption</figcaption>
+                    </figure>
+                </div>
+                <div class="justify-content-center spacer-slider">
+                    <figure class="figure"><img class="img-fluid figure-img" src="https://picsum.photos/1080/1335?image=1074" alt="alt text here">
+                        <figcaption class="figure-caption">Caption</figcaption>
+                    </figure>
+                </div>
+                <div class="justify-content-center spacer-slider">
+                    <figure class="figure"><img class="img-fluid figure-img" src="https://picsum.photos/1080/1335?image=1071" alt="alt text here">
+                        <figcaption class="figure-caption">Caption</figcaption>
+                    </figure>
+                </div>
+                <div class="justify-content-center spacer-slider">
+                    <figure class="figure"><img class="img-fluid figure-img" src="https://picsum.photos/1080/1335?image=1060" alt="alt text here">
+                        <figcaption class="figure-caption">Caption</figcaption>
+                    </figure>
+                </div>
+                <div class="justify-content-center spacer-slider">
+                    <figure class="figure"><img class="img-fluid figure-img d-flex" src="https://picsum.photos/1080/1335?image=1045" alt="alt text here">
+                        <figcaption class="figure-caption">Caption</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </section>
+        <div class="carousel slide" data-ride="carousel" data-interval="10000" id="carousel-t">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="col-9 text-center mx-auto testimonial-content"><img class="rounded-circle" src="assets/img/test-man.jpg" width="100">
+                        <p class="text-center rating">5&nbsp;<i class="icon ion-star"></i></p>
+                        <p class="text-center" style="color: var(--dark);"><em>"No sabía esquiar, la única nieve estaba en mi nariz."</em><br></p>
+                        <p class="signature" style="color: var(--gray);">Aitor Menta</p>
+                        <p class="text-center date" style="color: var(--gray);">Viaje a la Nieve Abril 2014<br></p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-9 offset-xl-1 text-center mx-auto testimonial-content"><img class="rounded-circle" src="assets/img/test-woman.jpg" width="100">
+                        <p class="text-center rating">5&nbsp;<i class="fa fa-star"></i></p>
+                        <p class="text-center" style="color: var(--gray);"><em>"Lorem ipsum dolor sit amet, nec cu omnium ponderum instructior, eligendi gubergren."</em><br></p>
+                        <p class="signature" style="color: var(--gray);">Mery Jane&nbsp;</p>
+                        <p class="text-center date" style="color: var(--gray);">Viaje a Tomorrowland, April&nbsp; 2012<br></p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-9 text-center mx-auto testimonial-content"><img class="rounded-circle" src="assets/img/test-male-pic.jpg" width="100">
+                        <p class="text-center rating">5&nbsp;<i class="fa fa-star"></i></p>
+                        <p class="text-center"><em>"Lorem ipsum dolor sit amet, nec cu omnium ponderum instructior, eligendi gubergren cotidieque te eam. Sed ceteros salutatus definiebas eu, ut modo argumentum reprimique quo. Per te convenire facilisis. Eu vel noster scaevola molestiae.&nbsp;Lorem ipsum dolor sit amet, nec cu omnium ponderum instructior, eligendi gubergren cotidieque te eam. Sed ceteros salutatus definiebas eu, ut modo argumentum reprimique quo. Per te convenire facilisis. Eu vel noster scaevola molestiae."</em><br></p>
+                        <p class="signature">John D.</p>
+                        <p class="text-center date">April 21, 2014<br></p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-9 offset-xl-1 text-center mx-auto testimonial-content"><img class="rounded-circle" src="assets/img/test-female-pic.jpg" width="100">
+                        <p class="text-center rating">5&nbsp;<i class="fa fa-star"></i></p>
+                        <p class="text-center"><em>"Lorem ipsum dolor sit amet, nec cu omnium ponderum instructior, eligendi gubergren cotidieque te eam. Sed ceteros salutatus definiebas eu, ut modo argumentum reprimique quo. Per te convenire facilisis. Eu vel noster scaevola molestiae.&nbsp;Lorem ipsum dolor sit amet, nec cu omnium ponderum instructior, eligendi gubergren cotidieque te eam. Sed ceteros salutatus definiebas eu, ut modo argumentum reprimique quo. Per te convenire facilisis. Eu vel noster scaevola molestiae."</em><br></p>
+                        <p class="signature">Jane D.</p>
+                        <p class="text-center date">April 21, 2014<br></p>
+                    </div>
+                </div>
+            </div>
+            <div><a class="carousel-control-prev" href="#carousel-t" role="button" data-slide="prev"><i class="icon ion-android-arrow-dropleft-circle d-flex d-lg-flex justify-content-center align-items-center"></i><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-t" role="button" data-slide="next"><i class="icon ion-android-arrow-dropright-circle d-flex d-lg-flex justify-content-center align-items-center"></i><span class="sr-only">Next</span></a></div>
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-t" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-t" data-slide-to="1"></li>
+                <li data-target="#carousel-t" data-slide-to="2"></li>
+                <li data-target="#carousel-t" data-slide-to="3"></li>
+            </ol>
+        </div>
+    </div>
 
     <?php include "com/pieDePagina.php"?>
 
