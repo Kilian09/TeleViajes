2c7803ec699f3fa33a2cc073e5c1b8a23adacd57
Kilian <kilian.garcia106@alu.ulpgc>
2020-12-17 18:03:19 +0000

    Cambios en cruceros.php

--- resources/views/cruceros.php
+++ resources/views/cruceros.php
@@ -4,39 +4,32 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>TELEVIAJES 1.6</title>
+    <title>Cruceros</title>
 
-    <?php include "com/estilos.php" ?>
+    <?php include "com/estilos.php"?>
+    <?php include "com/scripts.php"?>
 
 </head>
-
 <body>
 
-    <?php include "com/cabecera.php" ?>
+<?php include "com/cabecera.php"?>
 
     <section>
         <div id="w_shop_105" class="carousel slide w_shop_105_indicators w_shop_105_control_button thumb_scroll_x swipe_x ps_easeOutInCubic" data-ride="carousel" data-pause="hover" data-interval="8000" data-duration="2000">
             <div class="w_shop_105_main_header">
-                <h1><span>Cruceros Destacados</span></h1>
+                <h1><span>Cruceros</span><span>Destacados</span></h1>
             </div>
-
             <ol class="carousel-indicators">
                 <li class="active" data-target="#w_shop_105" data-slide-to="0" style="filter: blur(0px);"></li>
                 <li data-target="#w_shop_105" data-slide-to="1"></li>
                 <li data-target="#w_shop_105" data-slide-to="2"></li>
             </ol>
-
             <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active">
-                    <img src="assets/img/medi.jpg" alt="w_shop_105_01" height="350" width="500">
-
-                    <div class="w_shop_105_left_box">
-                        <span data-animation="animated fadeInLeft" style="font-family: roboto;">535.00€ por persona<br></span>
+                <div class="carousel-item active"><img src="assets/img/medi.jpg" alt="w_shop_105_01" height="350" width="500">
+                    <div class="w_shop_105_left_box"><span data-animation="animated fadeInLeft" style="font-family: roboto;">535.00€<br></span>
                         <h1 class="left-h" data-animation="animated fadeInLeft">5 maravillas del mediterráneo</h1>
-                        <p data-animation="animated fadeInLeft">Lorem ipsum dolor sit amet onsectetuer adipiscing elit unc varius facilisis eros in velit quis arcu ornare laoreet urabitur adipiscing luctus massa.</p>
-                        <a href="#" data-animation="animated fadeInLeft">Comprar</a>
+                        <p data-animation="animated fadeInLeft">Lorem ipsum dolor sit amet onsectetuer adipiscing elit unc varius facilisis eros in velit quis arcu ornare laoreet urabitur adipiscing luctus massa.</p><a href="#" data-animation="animated fadeInLeft">Comprar</a>
                     </div>
-
                     <div class="w_shop_105_right_box" data-animation="animated fadeInRight">
                         <ul>
                             <li data-animation="animated fadeInRight">Salida desde barcelona</li>
@@ -47,37 +40,26 @@
                         </ul>
                     </div>
                 </div>
-
-                <div class="carousel-item">
-                    <img src="assets/img/cruce2.jpg" alt="w_shop_105_02">
-
-                    <div class="w_shop_105_left_box">
-                        <span data-animation="animated fadeInLeft" style="font-family: roboto;">830.00€ por persona</span>
-                        <h1 class="left-h" data-animation="animated fadeInLeft">Crucero por los Fiordos</h1>
-                        <p data-animation="animated fadeInLeft">Lorem ipsum dolor sit amet onsectetuer adipiscing elit unc varius facilisis eros in velit quis arcu ornare laoreet urabitur adipiscing luctus massa.</p>
-                        <a href="#" data-animation="animated fadeInLeft">Comprar</a>
+                <div class="carousel-item"><img src="assets/img/cruce2.jpg" alt="w_shop_105_02">
+                    <div class="w_shop_105_left_box"><span data-animation="animated fadeInLeft" style="font-family: roboto;">$535.00</span>
+                        <h1 class="left-h" data-animation="animated fadeInLeft">product name and modal number</h1>
+                        <p data-animation="animated fadeInLeft">Lorem ipsum dolor sit amet onsectetuer adipiscing elit unc varius facilisis eros in velit quis arcu ornare laoreet urabitur adipiscing luctus massa.</p><a href="#" data-animation="animated fadeInLeft">order now</a>
                     </div>
-
                     <div class="w_shop_105_right_box" data-animation="animated fadeInRight">
                         <ul>
-                            <li data-animation="animated fadeInRight">Salida desde Bergen</li>
-                            <li data-animation="animated fadeInRight">5 Ciudades</li>
-                            <li data-animation="animated fadeInRight">2 semanas</li>
-                            <li data-animation="animated fadeInRight">Verano nórdico</li>
-                            <li data-animation="animated fadeInRight">Para todas las edades</li>
+                            <li data-animation="animated fadeInRight">some features</li>
+                            <li data-animation="animated fadeInRight">some other features</li>
+                            <li data-animation="animated fadeInRight">some more features</li>
+                            <li data-animation="animated fadeInRight">other features</li>
+                            <li data-animation="animated fadeInRight">features and features</li>
                         </ul>
                     </div>
                 </div>
-
-                <div class="carousel-item">
-                    <img src="assets/img/w_shop_105_03.png" alt="w_shop_105_03">
-                    <div class="w_shop_105_left_box">
-                        <span data-animation="animated fadeInLeft" style="font-family: roboto;">400.00€ por persona</span>
+                <div class="carousel-item"><img src="assets/img/w_shop_105_03.png" alt="w_shop_105_03">
+                    <div class="w_shop_105_left_box"><span data-animation="animated fadeInLeft" style="font-family: roboto;">$535.00</span>
                         <h1 class="left-h" data-animation="animated fadeInLeft">product name and modal number</h1>
-                        <p data-animation="animated fadeInLeft">Lorem ipsum dolor sit amet onsectetuer adipiscing elit unc varius facilisis eros in velit quis arcu ornare laoreet urabitur adipiscing luctus massa.</p>
-                        <a href="#" data-animation="animated fadeInLeft">Comprar</a>
+                        <p data-animation="animated fadeInLeft">Lorem ipsum dolor sit amet onsectetuer adipiscing elit unc varius facilisis eros in velit quis arcu ornare laoreet urabitur adipiscing luctus massa.</p><a href="#" data-animation="animated fadeInLeft">order now</a>
                     </div>
-
                     <div class="w_shop_105_right_box" data-animation="animated fadeInRight">
                         <ul>
                             <li data-animation="animated fadeInRight">some features</li>
@@ -92,8 +74,7 @@
         </div>
     </section>
 
-    <?php include "com/scripts.php" ?>
-    <?php include "com/pieDePagina.php" ?>
+    <?php include "com/pieDePagina.php"?>
 
 </body>
 
