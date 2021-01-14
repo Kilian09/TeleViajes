a090e8562f07f2364341d0d98bd9df68c984c683
Fernando Santana <feersantana5@gmail.com>
2021-01-14 23:53:15 +0000

    nuevos assests

--- resources/views/com/estilos.php
+++ resources/views/com/estilos.php
@@ -1,7 +1,17 @@
+
+
+
+
+
+
+
+
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin:700">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,600,800">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,400i,700,700i">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
