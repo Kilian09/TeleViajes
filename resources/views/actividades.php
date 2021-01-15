168abe77dd626207afe6407e6d2481d31cbdcd4d
Elias <elias.perez102@alu.ulpgc.es>
2021-01-15 18:42:59 +0000

    Descripcion activ

--- resources/views/actividades.php
+++ resources/views/actividades.php
@@ -23,7 +23,7 @@
                     <div class="box-img"><img src="assets/img/esqui.jpg" alt="Montañismo" style="text-align: center;"></div>
                     <div class="box-content">
                         <h4 class="title">Esqui</h4>
-                        <p class="description">Rellenar descripción</p>
+                        <p class="description">Patina con tu grupo de amigos en los fabulos pirineos españoles. Los Pirineos</p>
                         <ul class="social-links" style="text-align: center;">
                             <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
                         </ul>
@@ -35,7 +35,7 @@
                     <div class="box-img"><img src="assets/img/caribe.jpg" alt="Williamson"></div>
                     <div class="box-content">
                         <h4 class="title">Playas EXÓTICAS<br></h4>
-                        <p class="description">Rellenar Descripcion</p>
+                        <p class="description">Disfruta de las mejores playas del Caribe, donde desconectar de la rutina. El Caribe</p>
                         <ul class="social-links" style="text-align: center;">
                             <li style="text-align: left;"><a href="#" style="font-size: 30px;"><i class="fa fa-shopping-cart"></i></a></li>
                         </ul>
@@ -47,7 +47,7 @@
                     <div class="box-img"><img src="assets/img/visitas%20guiadas.jpg" alt="Williamson"></div>
                     <div class="box-content">
                         <h4 class="title">visitas guiadas<br></h4>
-                        <p class="description">Rellenar descripción</p>
+                        <p class="description">Descubre la historia de los antepasados españoles y todas sus costumbres. Sevilla</p>
                         <ul class="social-links" style="text-align: center;">
                             <li><a href="#"><i class="fa fa-shopping-cart" style="text-align: right;font-size: 30px;"></i></a></li>
                         </ul>
@@ -64,7 +64,7 @@
                     <div class="box-img"><img src="assets/img/lasvegas.jpg" alt="Montañismo" style="text-align: center;"></div>
                     <div class="box-content">
                         <h4 class="title">Casinos</h4>
-                        <p class="description">Rellenar descripción</p>
+                        <p class="description">Disfruta de los mejores casinos del mundo. Las Vegas</p>
                         <ul class="social-links" style="text-align: center;">
                             <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
                         </ul>
@@ -76,7 +76,7 @@
                     <div class="box-img"><img src="assets/img/spa.jpg" alt="Williamson"></div>
                     <div class="box-content">
                         <h4 class="title">Spa<br></h4>
-                        <p class="description">Rellenar Descripcion</p>
+                        <p class="description">Sitio ideal para relajarte y desconectar de la rutina. SPA</p>
                         <ul class="social-links" style="text-align: center;">
                             <li style="text-align: left;"><a href="#" style="font-size: 30px;"><i class="fa fa-shopping-cart"></i></a></li>
                         </ul>
@@ -88,7 +88,7 @@
                     <div class="box-img"><img src="assets/img/cena.jpeg" alt="Williamson"></div>
                     <div class="box-content">
                         <h4 class="title">Cena exclusiva<br></h4>
-                        <p class="description">Rellenar descripción</p>
+                        <p class="description">Disfruta de una cena con tú mujer desde un octavo piso con las mejores vistas de la ciudad</p>
                         <ul class="social-links" style="text-align: center;">
                             <li><a href="#"><i class="fa fa-shopping-cart" style="text-align: right;font-size: 30px;"></i></a></li>
                         </ul>
@@ -105,7 +105,7 @@
                     <div class="box-img"><img src="assets/img/escalada.jpg" alt="Montañismo" style="text-align: center;"></div>
                     <div class="box-content">
                         <h4 class="title">Escalada</h4>
-                        <p class="description">Rellenar descripción</p>
+                        <p class="description">¿Buscas nuevos retos? Que te parece subir una pared vertical.</p>
                         <ul class="social-links" style="text-align: center;">
                             <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
                         </ul>
@@ -117,7 +117,7 @@
                     <div class="box-img"><img src="assets/img/paracaidismo.jpg" alt="Williamson"></div>
                     <div class="box-content">
                         <h4 class="title">Paracaidismo<br></h4>
-                        <p class="description">Rellenar Descripcion</p>
+                        <p class="description">¿Siempre has querido sentir la sensacion de volar? Solo hace falta un salto</p>
                         <ul class="social-links" style="text-align: center;">
                             <li style="text-align: left;"><a href="#" style="font-size: 30px;"><i class="fa fa-shopping-cart"></i></a></li>
                         </ul>
@@ -129,7 +129,7 @@
                     <div class="box-img"><img src="assets/img/paintball.jpg" alt="Williamson"></div>
                     <div class="box-content">
                         <h4 class="title">Paintball<br></h4>
-                        <p class="description">Rellenar descripción</p>
+                        <p class="description">¿Que tal una tarde con los amigos diferente? Con el Paintball teneis diversion asegurada.</p>
                         <ul class="social-links" style="text-align: center;">
                             <li><a href="#"><i class="fa fa-shopping-cart" style="text-align: right;font-size: 30px;"></i></a></li>
                         </ul>
