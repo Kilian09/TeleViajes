161ffb66fb69e35dbb3fa9400d3ddf56f2d29787
Kilian <kilian.garcia106@alu.ulpgc>
2021-01-15 17:02:49 +0000

    Añadida tabla de paquetes

---
+++ database/migrations/2021_01_15_165840_create_paquetes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaquetesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paquetes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_user')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users')->onDelete("cascade");
+            $table->string('name');
+            $table->string('descripction');
+            $table->string('type_product');
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paquetes');
+    }
+}
