f477f9e5302f4c944457cb1966aa928f3763506f
Fernando Santana <feersantana5@gmail.com>
2021-01-15 00:41:58 +0000

    Actualizacion index

--- resources/views/index.php
+++ resources/views/index.php
@@ -21,7 +21,7 @@
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
                         <h1 class="brand-heading">TELEVIAJES</h1>
-                        <p class="intro-text">En 2020 todo tiene que tener tele delante.</p><a class="btn btn-link btn-circle" role="button" href="#carousel"><i class="fa fa-angle-double-down animated"></i></a>
+                        <p class="intro-text">En esta época todo tiene que tener tele delante.</p><a class="btn btn-link btn-circle" role="button" href="#carousel"><i class="fa fa-angle-double-down animated"></i></a>
                     </div>
                 </div>
             </div>
@@ -30,55 +30,60 @@
     <div class="features-boxed">
         <div class="container">
             <div class="intro">
-                <h2 class="text-center">Features </h2>
+                <h2 class="text-center">ORGANIZA TU VIAJE</h2>
                 <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae.</p>
             </div>
             <div class="row justify-content-center features">
+
                 <div class="col-sm-6 col-md-5 col-lg-4 item">
-                    <div class="box"><i class="fa fa-map-marker icon"></i>
-                        <h3 class="name">Works everywhere</h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a>
+                    <div class="box"><i class="fa fa-leaf icon"></i>
+                        <h3 class="name">Naturaleza </h3>
+                        <p>En nuestra oferta destacan nuestras actividades en la naturaleza y los destinos idílicos.</p><a class="learn-more" href="actividades">Ver Actividades »</a>
                     </div>
                 </div>
                 <div class="col-sm-6 col-md-5 col-lg-4 item">
                     <div class="box"><i class="fa fa-clock-o icon"></i>
-                        <h3 class="name">Always available</h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a>
+                        <h3 class="name">Durante todo el año</h3>
+                        <p>Desde Enero hasta Diciembre, desde la playa hasta la nieve, de Canarias al mundo.</p><a class="learn-more" href="#carousel">Ver Oferta »</a>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div class="box"><i class="fa fa-plane icon"></i>
+                        <h3 class="name">Velocidad</h3>
+                        <p>Los viajes más baratos con las menores y más rápidas conexiones posibles.</p><a class="learn-more" href="#novedadesIndex">Ver Oferta »</a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div class="box"><i class="fa fa-map-marker icon"></i>
+                        <h3 class="name">A cualquier lugar</h3>
+                        <p >En caso de que nuestras ofertas no se ajusten a tus gustos, buscaremos tu destino ideal.</p><a class="learn-more" href="contacto">Contáctanos »</a>
                     </div>
                 </div>
                 <div class="col-sm-6 col-md-5 col-lg-4 item">
                     <div class="box"><i class="fa fa-list-alt icon"></i>
-                        <h3 class="name">Customizable </h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a>
+                        <h3 class="name">Máxima Flexibilidad </h3>
+                        <p>En caso de que nuestras ofertas no se ajusten a su calendario, buscaremos una solución.</p><a class="learn-more" href="contacto">Contáctanos »</a>
                     </div>
                 </div>
-                <div class="col-sm-6 col-md-5 col-lg-4 item">
-                    <div class="box"><i class="fa fa-leaf icon"></i>
-                        <h3 class="name">Organic </h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-5 col-lg-4 item">
-                    <div class="box"><i class="fa fa-plane icon"></i>
-                        <h3 class="name">Fast </h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a>
-                    </div>
-                </div>
+
                 <div class="col-sm-6 col-md-5 col-lg-4 item">
                     <div class="box"><i class="fa fa-phone icon"></i>
-                        <h3 class="name">Mobile-first</h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a>
+                        <h3 class="name">24/7</h3>
+                        <p>Nuestro equipo estará pendiente de usted en caso de cualquier incidencia.</p><a class="learn-more" href="contacto">Contáctanos »</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
     <div data-bs-parallax-bg="true" style="height:500px;background-image:url(https://unsplash.it/1800/900?image=1051);background-position:center;background-size:cover;"></div>
-    <div class="article-list">
+
+    <div class="article-list" id="novedadesIndex">
         <div class="container">
             <div class="intro">
-                <h2 class="text-center" style="font-size: 30px;text-align: center;color: var(--danger);">Últimas Ofertas y novedades</h2>
-                <p class="text-center">Recopilatorio de nuestras últimas novedades y productos en oferta de nuestra web.</p>
+                <h2 class="text-center" style="font-size: 30px;text-align: center;color: var(--danger);">Vuelos Calientes (PONER VUELOS Y ORDENAR LO MAS BARATO PRIMERO)</h2>
+                <p class="text-center">Recopilatorio de los vuelos mas económicos actualmente.</p>
             </div>
             <div class="row articles">
                 <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/desk.jpg"></a>
@@ -124,6 +129,7 @@
             </div>
         </div>
     </div>
+
     <section id="carousel">
         <div class="carousel slide" data-ride="carousel" id="carousel-1">
             <div class="carousel-inner">
@@ -165,7 +171,7 @@
             </ol>
         </div>
     </section>
-    <div class="footer-dark" style="padding: 5px;"></div>
+
 
 <?php include "com/pieDePagina.php"?>
 
