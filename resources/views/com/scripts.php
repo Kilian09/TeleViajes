c2745dfa21abc8accff6b13b870cf548ea5e94d7
Fernando Santana <feersantana5@gmail.com>
2021-01-15 21:04:32 +0000

    añadido ajax

--- resources/views/com/scripts.php
+++ resources/views/com/scripts.php
@@ -5,4 +5,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
 <script src="assets/js/script.min.js"></script>
+<script src="assets/js/ajax.js"></script>
 
