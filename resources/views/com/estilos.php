5045bcb2fa3cc681a1108463d4256b755df0f3ad
Fernando Santana <feersantana5@gmail.com>
2021-01-15 13:44:04 +0000

    styling

--- resources/views/com/estilos.php
+++ resources/views/com/estilos.php
@@ -7,6 +7,9 @@
 
 
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+<link rel="stylesheet" href="assets/css/styles.min.css">
+
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin:700">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
@@ -15,7 +18,5 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-<link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
-<link rel="stylesheet" href="assets/css/styles.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
