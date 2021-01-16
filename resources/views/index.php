3d2a16ea308686a1ea117b49758a3a6b95f28b04
Fernando Santana <feersantana5@gmail.com>
2021-01-16 19:10:56 +0000

    vuelos mini

--- resources/views/index.php
+++ resources/views/index.php
@@ -97,10 +97,12 @@
                 <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/desk.jpg"></a>
                     <h3 class="name"><?php echo $vuelo->name ?></h3>
                     <p class="description"><?php echo $vuelo->description ?>. Desde <?php echo $vuelo->date?>. Por <?php echo $vuelo->price?>. Quedan <?php echo $vuelo->stock ?> plazas. </p>
+
                     <form method="get" action="/addCart/<?php echo $product->id ?>">
                     <input type="number" name="amount" value="1">
                     <button type="submit" class="fas fa-shopping-cart" style="color: var(--danger);"></button>
                     </form>
+
                 </div>
 
             <?php }}}}} ?>
