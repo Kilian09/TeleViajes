6b81eb43fb29076332b644b4d05970ad59f69cb3
Fernando Santana <feersantana5@gmail.com>
2021-01-16 11:52:42 +0000

    vista con productos

--- resources/views/actividades.php
+++ resources/views/actividades.php
@@ -18,125 +18,86 @@
     <div class="container" style="padding-top: 30px;">
         <h1>las actividades más vendidas</h1>
         <div class="row">
+
+            <?php
+            if (isset($products, $activities)){
+            foreach ($products as $product){
+            if($product->type == "Actividades"){
+            foreach ($activities as $activity){
+            if($product->id == $activity->id_product) {
+                if($activity->type == "Populares") {  ?>
+
             <div class="col-sm-6 col-md-4">
                 <div class="box">
                     <div class="box-img"><img src="assets/img/esqui.jpg" alt="Montañismo" style="text-align: center;"></div>
                     <div class="box-content">
-                        <h4 class="title">Esqui</h4>
-                        <p class="description">Patina con tu grupo de amigos en los fabulos pirineos españoles. Los Pirineos</p>
+                        <h4 class="title"><?php echo $activity->name ?></h4>
+                        <p class="description"><?php echo $activity->description ?>. Por <?php echo $activity->price?>. <?php echo $activity->date?>. Quedan <?php echo $activity->stock?> plazas libres.</p>
                         <ul class="social-links" style="text-align: center;">
                             <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="box">
-                    <div class="box-img"><img src="assets/img/caribe.jpg" alt="Williamson"></div>
-                    <div class="box-content">
-                        <h4 class="title">Playas EXÓTICAS<br></h4>
-                        <p class="description">Disfruta de las mejores playas del Caribe, donde desconectar de la rutina. El Caribe</p>
-                        <ul class="social-links" style="text-align: center;">
-                            <li style="text-align: left;"><a href="#" style="font-size: 30px;"><i class="fa fa-shopping-cart"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="box">
-                    <div class="box-img"><img src="assets/img/visitas%20guiadas.jpg" alt="Williamson"></div>
-                    <div class="box-content">
-                        <h4 class="title">visitas guiadas<br></h4>
-                        <p class="description">Descubre la historia de los antepasados españoles y todas sus costumbres. Sevilla</p>
-                        <ul class="social-links" style="text-align: center;">
-                            <li><a href="#"><i class="fa fa-shopping-cart" style="text-align: right;font-size: 30px;"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+            <?php }}}}}} ?>
+
     </div>
+
     <div class="container" style="padding-top: 30px;">
         <h1>las Actividades exclusivas</h1>
         <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <div class="box">
-                    <div class="box-img"><img src="assets/img/lasvegas.jpg" alt="Montañismo" style="text-align: center;"></div>
-                    <div class="box-content">
-                        <h4 class="title">Casinos</h4>
-                        <p class="description">Disfruta de los mejores casinos del mundo. Las Vegas</p>
-                        <ul class="social-links" style="text-align: center;">
-                            <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
-                        </ul>
+            <?php
+            if (isset($products, $activities)){
+            foreach ($products as $product){
+            if($product->type == "Actividades"){
+            foreach ($activities as $activity){
+            if($product->id == $activity->id_product) {
+            if($activity->type == "Exclusivas") {  ?>
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="box">
+                        <div class="box-img"><img src="assets/img/esqui.jpg" alt="Montañismo" style="text-align: center;"></div>
+                        <div class="box-content">
+                            <h4 class="title"><?php echo $activity->name ?></h4>
+                            <p class="description"><?php echo $activity->description ?>. Por <?php echo $activity->price?>. <?php echo $activity->date?>. Quedan <?php echo $activity->stock?> plazas libres.</p>
+                            <ul class="social-links" style="text-align: center;">
+                                <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="box">
-                    <div class="box-img"><img src="assets/img/spa.jpg" alt="Williamson"></div>
-                    <div class="box-content">
-                        <h4 class="title">Spa<br></h4>
-                        <p class="description">Sitio ideal para relajarte y desconectar de la rutina. SPA</p>
-                        <ul class="social-links" style="text-align: center;">
-                            <li style="text-align: left;"><a href="#" style="font-size: 30px;"><i class="fa fa-shopping-cart"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="box">
-                    <div class="box-img"><img src="assets/img/cena.jpeg" alt="Williamson"></div>
-                    <div class="box-content">
-                        <h4 class="title">Cena exclusiva<br></h4>
-                        <p class="description">Disfruta de una cena con tú mujer desde un octavo piso con las mejores vistas de la ciudad</p>
-                        <ul class="social-links" style="text-align: center;">
-                            <li><a href="#"><i class="fa fa-shopping-cart" style="text-align: right;font-size: 30px;"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+            <?php }}}}}} ?>
     </div>
+
     <div class="container" style="padding-top: 30px;">
         <h1>las actividades más salvajes</h1>
         <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <div class="box">
-                    <div class="box-img"><img src="assets/img/escalada.jpg" alt="Montañismo" style="text-align: center;"></div>
-                    <div class="box-content">
-                        <h4 class="title">Escalada</h4>
-                        <p class="description">¿Buscas nuevos retos? Que te parece subir una pared vertical.</p>
-                        <ul class="social-links" style="text-align: center;">
-                            <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="box">
-                    <div class="box-img"><img src="assets/img/paracaidismo.jpg" alt="Williamson"></div>
-                    <div class="box-content">
-                        <h4 class="title">Paracaidismo<br></h4>
-                        <p class="description">¿Siempre has querido sentir la sensacion de volar? Solo hace falta un salto</p>
-                        <ul class="social-links" style="text-align: center;">
-                            <li style="text-align: left;"><a href="#" style="font-size: 30px;"><i class="fa fa-shopping-cart"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="box">
-                    <div class="box-img"><img src="assets/img/paintball.jpg" alt="Williamson"></div>
-                    <div class="box-content">
-                        <h4 class="title">Paintball<br></h4>
-                        <p class="description">¿Que tal una tarde con los amigos diferente? Con el Paintball teneis diversion asegurada.</p>
-                        <ul class="social-links" style="text-align: center;">
-                            <li><a href="#"><i class="fa fa-shopping-cart" style="text-align: right;font-size: 30px;"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+            <?php
+            if (isset($products, $activities)){
+                foreach ($products as $product){
+                    if($product->type == "Actividades"){
+                        foreach ($activities as $activity){
+                            if($product->id == $activity->id_product) {
+                                if($activity->type == "Salvajes") {  ?>
+
+                                    <div class="col-sm-6 col-md-4">
+                                        <div class="box">
+                                            <div class="box-img"><img src="assets/img/esqui.jpg" alt="Montañismo" style="text-align: center;"></div>
+                                            <div class="box-content">
+                                                <h4 class="title"><?php echo $activity->name ?></h4>
+                                                <p class="description"><?php echo $activity->description ?>. Por <?php echo $activity->price?>. <?php echo $activity->date?>. Quedan <?php echo $activity->stock?> plazas libres.</p>
+                                                <ul class="social-links" style="text-align: center;">
+                                                    <li><a href="#"><i class="fa fa-shopping-cart" style="font-size: 30px;"></i></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                <?php }}}}}} ?>
+
     </div>
 
     <?php include "com/pieDePagina.php"?>
