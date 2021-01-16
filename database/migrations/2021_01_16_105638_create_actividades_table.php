c6d6308779363bb9b35fc44deffd23d19766db2d
Fernando Santana <feersantana5@gmail.com>
2021-01-16 10:57:53 +0000

    actividades tabla y modelo

---
+++ database/migrations/2021_01_16_105638_create_actividades_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActividadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actividades', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_product')->unsigned();
+            $table->foreign('id_product')->references('id')->on('products')->onDelete("cascade");
+            $table->string('name');
+            $table->string('description');
+            $table->string('type');
+            $table->double('price');
+            $table->double('stock');
+            $table->string('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actividades');
+    }
+}
