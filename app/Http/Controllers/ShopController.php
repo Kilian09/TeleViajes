5a9a155af1ce0967da28a33ff9cc6c624222f9a7
Kilian <kilian.garcia106@alu.ulpgc>
2021-01-17 15:24:34 +0000

    Cambios en el eliminarProductoCarrito

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -220,185 +220,104 @@ class ShopController extends Controller
     public function eliminarProductoCarrito($id)
     {
         $producto = Products::where('id', $id)->get();
+        $typeProduct = $producto[0]['type'];
 
+        session(['amountProducto' => session('PROD_' . $id)]);
+        session()->forget('PROD_' . $id);
 
+        $totalPrice = session('total');
 
+        switch ($typeProduct) {
 
-        if ($producto[0]['type'] == "Paquetes") {
+            case "Paquetes":
+                $paquete = Paquetes::where('id_product', $producto[0]['id'])->get();
 
-            session(['amountProducto' => session('PROD_' . $id)]);
+                $precioARestar = session('amountProducto') * $paquete[0]['price'];
+                break;
 
-            session()->forget('PROD_' . $id);
 
-            $paquete = Paquetes::where('id_product', $producto[0]['id'])->get();
+            case "Cruceros":
+                $crucero = Cruceros::where('id_product', $producto[0]['id'])->get();
 
-            $totalPrice = session('total');
-            $precioARestar = session('amountProducto') * $paquete[0]['price'];
-            $result = $totalPrice - $precioARestar;
-            session(['totalAnterior' => $result]);
-            session(['total' => $result]);
+                $precioARestar = session('amountProducto') * $crucero[0]['price'];
+                break;
 
-            $cantidadFinal = session('cantidadTotal');
-            $result = $cantidadFinal - session('amountProducto');
-            session(['cantidadAnterior' => $result]);
-            session(['cantidadTotal' => $result]);
+            case "Actividades":
+                $actividades = Actividades::where('id_product', $producto[0]['id'])->get();
 
-            return redirect('/paquetes')->with('exito', 'Se ha eliminado el producto del carrito.');
+                $precioARestar = session('amountProducto') * $actividades[0]['price'];
+                break;
 
-        } else if ($producto[0]['type'] == "Cruceros") {
+            case "Escolares":
+                $escolares = Escolares::where('id_product', $producto[0]['id'])->get();
 
-            session(['amountProducto' => session('PROD_' . $id)]);
+                $precioARestar = session('amountProducto') * $escolares[0]['price'];
+                break;
 
-            session()->forget('PROD_' . $id);
 
-            $crucero = Cruceros::where('id_product', $producto[0]['id'])->get();
+            case "Universitarios":
+                $universitarios = Universitarios::where('id_product', $producto[0]['id'])->get();
 
-            $totalPrice = session('total');
-            $precioARestar = session('amountProducto') * $crucero[0]['price'];
-            $result = $totalPrice - $precioARestar;
-            session(['totalAnterior' => $result]);
-            session(['total' => $result]);
+                $precioARestar = session('amountProducto') * $universitarios[0]['price'];
+                break;
 
-            $cantidadFinal = session('cantidadTotal');
+            case "Vuelos":
+                $vuelos = Vuelos::where('id_product', $producto[0]['id'])->get();
 
-            $result = $cantidadFinal - session('amountProducto');
-            session(['cantidadAnterior' => $result]);
-            session(['cantidadTotal' => $result]);
+                $precioARestar = session('amountProducto') * $vuelos[0]['price'];
+                break;
 
-            return redirect('/cruceros')->with('exito', 'Se ha eliminado el producto del carrito.');
+            case "Ancianos":
+                $ancianos = Ancianos::where('id_product', $producto[0]['id'])->get();
 
-        } else if ($producto[0]['type'] == "Actividades") {
+                $precioARestar = session('amountProducto') * $ancianos[0]['price'];
+                break;
 
-            session(['amountProducto' => session('PROD_' . $id)]);
+            case "Familia":
+                $familia = Familia::where('id_product', $producto[0]['id'])->get();
 
-            session()->forget('PROD_' . $id);
+                $precioARestar = session('amountProducto') * $familia[0]['price'];
+                break;
 
-            $actividades = Actividades::where('id_product', $producto[0]['id'])->get();
-
-            $totalPrice = session('total');
-            $precioARestar = session('amountProducto') * $actividades[0]['price'];
-            $result = $totalPrice - $precioARestar;
-            session(['totalAnterior' => $result]);
-            session(['total' => $result]);
-
-            $cantidadFinal = session('cantidadTotal');
-
-            $result = $cantidadFinal - session('amountProducto');
-            session(['cantidadAnterior' => $result]);
-            session(['cantidadTotal' => $result]);
-
-            return redirect('/actividades')->with('exito', 'Se ha eliminado el producto del carrito.');
-
-        } else if ($producto[0]['type'] == "Escolares") {
-
-            session(['amountProducto' => session('PROD_' . $id)]);
-
-            session()->forget('PROD_' . $id);
-
-            $escolares = Escolares::where('id_product', $producto[0]['id'])->get();
-
-            $totalPrice = session('total');
-            $precioARestar = session('amountProducto') * $escolares[0]['price'];
-            $result = $totalPrice - $precioARestar;
-            session(['totalAnterior' => $result]);
-            session(['total' => $result]);
-
-            $cantidadFinal = session('cantidadTotal');
-
-            $result = $cantidadFinal - session('amountProducto');
-            session(['cantidadAnterior' => $result]);
-            session(['cantidadTotal' => $result]);
-
-            return redirect('/escolares')->with('exito', 'Se ha eliminado el producto del carrito.');
-
-        } else if ($producto[0]['type'] == "Universitarios") {
-
-            session(['amountProducto' => session('PROD_' . $id)]);
-
-            session()->forget('PROD_' . $id);
-
-            $universitarios = Universitarios::where('id_product', $producto[0]['id'])->get();
-
-            $totalPrice = session('total');
-            $precioARestar = session('amountProducto') * $universitarios[0]['price'];
-            $result = $totalPrice - $precioARestar;
-            session(['totalAnterior' => $result]);
-            session(['total' => $result]);
-
-            $cantidadFinal = session('cantidadTotal');
-
-            $result = $cantidadFinal - session('amountProducto');
-            session(['cantidadAnterior' => $result]);
-            session(['cantidadTotal' => $result]);
-
-            return redirect('/universitarios')->with('exito', 'Se ha eliminado el producto del carrito.');
-
-        } else if ($producto[0]['type'] == "Vuelos") {
-
-            session(['amountProducto' => session('PROD_' . $id)]);
-
-            session()->forget('PROD_' . $id);
-
-            $vuelos = Vuelos::where('id_product', $producto[0]['id'])->get();
-
-            $totalPrice = session('total');
-            $precioARestar = session('amountProducto') * $vuelos[0]['price'];
-            $result = $totalPrice - $precioARestar;
-            session(['totalAnterior' => $result]);
-            session(['total' => $result]);
-
-            $cantidadFinal = session('cantidadTotal');
-
-            $result = $cantidadFinal - session('amountProducto');
-            session(['cantidadAnterior' => $result]);
-            session(['cantidadTotal' => $result]);
-
-            return redirect('/')->with('exito', 'Se ha eliminado el producto del carrito.');
-
-        } else if ($producto[0]['type'] == "Ancianos") {
-
-            session(['amountProducto' => session('PROD_' . $id)]);
-
-            session()->forget('PROD_' . $id);
-
-            $ancianos = Ancianos::where('id_product', $producto[0]['id'])->get();
-
-            $totalPrice = session('total');
-            $precioARestar = session('amountProducto') * $ancianos[0]['price'];
-            $result = $totalPrice - $precioARestar;
-            session(['totalAnterior' => $result]);
-            session(['total' => $result]);
-
-            $cantidadFinal = session('cantidadTotal');
-
-            $result = $cantidadFinal - session('amountProducto');
-            session(['cantidadAnterior' => $result]);
-            session(['cantidadTotal' => $result]);
-
-            return redirect('/ancianos')->with('exito', 'Se ha eliminado el producto del carrito.');
-
-        } else if ($producto[0]['type'] == "Familia") {
-
-            session(['amountProducto' => session('PROD_' . $id)]);
-
-            session()->forget('PROD_' . $id);
-
-            $familia = Familia::where('id_product', $producto[0]['id'])->get();
-
-            $totalPrice = session('total');
-            $precioARestar = session('amountProducto') * $familia[0]['price'];
-            $result = $totalPrice - $precioARestar;
-            session(['totalAnterior' => $result]);
-            session(['total' => $result]);
-
-            $cantidadFinal = session('cantidadTotal');
-
-            $result = $cantidadFinal - session('amountProducto');
-            session(['cantidadAnterior' => $result]);
-            session(['cantidadTotal' => $result]);
-
-            return redirect('/familia')->with('exito', 'Se ha eliminado el producto del carrito.');
         }
+
+        $result = $totalPrice - $precioARestar;
+        session(['totalAnterior' => $result]);
+        session(['total' => $result]);
+
+        $cantidadFinal = session('cantidadTotal');
+
+        $result = $cantidadFinal - session('amountProducto');
+        session(['cantidadAnterior' => $result]);
+        session(['cantidadTotal' => $result]);
+
+        switch ($typeProduct) {
+            case "Paquetes":
+                return redirect('/paquetes')->with('exito', 'Se ha eliminado el producto del carrito.');
+
+            case "Cruceros":
+                return redirect('/cruceros')->with('exito', 'Se ha eliminado el producto del carrito.');
+
+            case "Actividades":
+                return redirect('/actividades')->with('exito', 'Se ha eliminado el producto del carrito.');
+
+            case "Escolares":
+                return redirect('/escolares')->with('exito', 'Se ha eliminado el producto del carrito.');
+
+            case "Universitarios":
+                return redirect('/universitarios')->with('exito', 'Se ha eliminado el producto del carrito.');
+
+            case "Vuelos":
+                return redirect('/')->with('exito', 'Se ha eliminado el producto del carrito.');
+
+            case "Ancianos":
+                return redirect('/ancianos')->with('exito', 'Se ha eliminado el producto del carrito.');
+
+            case "Familia":
+                return redirect('/familia')->with('exito', 'Se ha eliminado el producto del carrito.');
+
+        }
+
     }
 
 }
