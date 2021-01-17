b9d6aa92d075672e80e689ea917a5cd0aece04ae
Kilian <kilian.garcia106@alu.ulpgc>
2021-01-17 19:04:39 +0000

    Version final pagina

--- resources/views/privacidad.php
+++ resources/views/privacidad.php
@@ -13,10 +13,10 @@
 
 <?php include "com/cabecera.php" ?>
 
-<header class="centrarHeader">
 
-        <h1>Nuestra Política de Privacidad </h1>
-</header>
+        <h1 id="centrarHeaderPrivacidad">Nuestra Política de Privacidad </h1>
+
+
 
 <section>
     <article class="articlePrivacidad">
@@ -26,48 +26,45 @@
             los principios y derechos recogidos en el Reglamento (UE) 2016/679,
             de 27 de abril, General de Protección de Datos (RGPD) y demás normativa aplicable. </p>
     </article>
-    <article class="articlePrivacidad">
-        <header class="centrarHeader">
-            <h2>Responsable</h2>
-        </header>
-        <h3>
+    <details  class="articlePrivacidad">
+        <summary class="centrarHeader" >Responsable</summary>
+        <h3 >
             <bold>Televiajes</bold>
         </h3>
         <p> Tafira Baja, Las Palmas de Gran Canaria, España </p>
         <p>Teléfono de contacto: +34 630 267876 </p>
-    </article>
-    <article class="articlePrivacidad">
-        <header class="centrarHeader">
-            <h2>¿Dónde se utilizan esos datos?</h2>
-        </header>
+    </details>
+    <details  class="articlePrivacidad">
+        <summary class="centrarHeader" >¿Dónde se utilizan esos datos?</summary>
         <p>Utilizaremos tus datos solamente para acceder a la pagina web a través del formulario login</p>
         <p>Televiajes asegura la confidencialidad de los datos aportados y garantiza que, en ningún caso, serán cedidos
             para ningún otro uso sin mediar consentimiento
             previo y expreso de nuestros usuarios. Sólo le pediremos aquellos datos necesarios para la prestación del
             servicio requerido y únicamente serán empleados para este fin.</p>
-    </article>
-    <article class="articlePrivacidad">
-        <header class="centrarHeader">
-            <h2>¿Dispones del consentimiento de los usuarios?</h2>
-        </header>
+
+    </details>
+    <details  class="articlePrivacidad">
+        <summary class="centrarHeader" >
+            ¿Dispones del consentimiento de los usuarios?
+        </summary>
         <p>Los usuarios para registrarse deberan leer y aceptar
             <bold>la polítia de privacidad</bold>
         </p>
-    </article>
-    <article class="articlePrivacidad">
-        <header class="centrarHeader">
-            <h2>¿Se tratan con fines comerciales?</h2>
-        </header>
+    </details>
+    <details  class="articlePrivacidad">
+        <summary class="centrarHeader" >
+            ¿Se tratan con fines comerciales?
+        </summary>
         <p>Sí, solo utilizaremos los datos de los usuarios para enviar publicidad y promociones por correo
             electrónico</p>
-    </article>
-    <article class="articlePrivacidad">
-        <header class="centrarHeader">
-            <h2>¿Se realizan cesiones a terceros o transferencias internacionales?</h2>
-        </header>
+        </details>
+    <details  class="articlePrivacidad">
+        <summary class="centrarHeader" >
+            ¿Se realizan cesiones a terceros o transferencias internacionales?
+        </summary>
         <p>No, nuestra página no realizará cesiones a terceros o transferencias internacionales</p>
 
-    </article>
+        </details>
 </section>
 
 
